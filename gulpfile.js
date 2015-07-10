4176d02159e317749501ed9f7d2f2fa2b56f18c2
Sabeur Thabti <sabeur.thabti@net-a-porter.com>
2015-07-10 16:26:26 +0100

    font versioning

--- gulpfile.js
+++ gulpfile.js
@@ -3,57 +3,68 @@ var rename = require("gulp-rename");
 var sketch = require("gulp-sketch");
 var iconfont = require('gulp-iconfont');
 var consolidate = require('gulp-consolidate');
-var clean = require('gulp-clean');
+var rimraf = require('rimraf');
 
-var fontName = 'mrpfont'; // set name of your symbol font
+
+var version = require('./package.json').version;
+var fontName = 'mrpfont-v'+version; // set name of your symbol font
 var template = 'mrporter-style'; // you can also choose 'foundation-style'
+var font = 'mrpfont';
 
 gulp.task('default',['clean'], function(){
   gulp.src('mrporter.sketch') // you can also choose 'symbol-font-16px.sketch'
-    .pipe(sketch({
-      export: 'artboards',
-      formats: 'svg'
-    }))
-    .pipe(iconfont({ fontName: fontName }))
-    .on('glyphs', function(glyphs) {
-      var options = {
-        glyphs: glyphs.map(function(glyph) {
-          // this line is needed because gulp-iconfont has changed the api from 2.0
-          return { name: glyph.name, codepoint: glyph.unicode[0].charCodeAt(0) }
-        }),
-        fontName: fontName,
-        fontPath: 'fonts/', // set path to font (from your CSS file if relative)
-        className: fontName // set class name in your CSS
-      };
+  .pipe(sketch({
+    export: 'artboards',
+    formats: 'svg'
+  }))
+  .pipe(iconfont({ fontName: fontName }))
+  .on('glyphs', function(glyphs) {
+
+    var options = {
+      glyphs: glyphs.map(function(glyph) {
+        // this line is needed because gulp-iconfont has changed the api from 2.0
+        return { name: glyph.name, codepoint: glyph.unicode[0].charCodeAt(0) }
+      }),
+      fontName: fontName,
+      fontPath: 'fonts/', // set path to font (from your CSS file if relative)
+      className: font // set class name in your CSS
+    };
 
 
-      gulp.src('templates/' + template + '.css')
-        .pipe(consolidate('lodash', options))
-        .pipe(rename({ basename:fontName }))
-        .pipe(gulp.dest('dist/css/')); // set path to export your CSS
+    gulp.src('templates/' + template + '.css')
+    .pipe(consolidate('lodash', options))
+    .pipe(rename({ basename:fontName }))
+    .pipe(gulp.dest('dist/css/')); // set path to export your CSS
 
-        gulp.src('templates/' + template + '.scss')
-          .pipe(consolidate('lodash', options))
-          .pipe(rename({ basename:fontName }))
-          .pipe(gulp.dest('dist/')); // set path to export your SCSS
-
-          console.log(options)
-
-
-      // if you don't need sample.html, remove next 4 lines
-      gulp.src('templates/' + template + '.html')
-        .pipe(consolidate('lodash', options))
-        .pipe(rename({ basename:'index' }))
-        .pipe(gulp.dest('dist/')); // set path to export your sample HTML
-    })
-    .pipe(gulp.dest('dist/fonts')) // set path to export your fonts
-        .pipe(gulp.dest('dist/css/fonts')); // set path to export your fonts
+    gulp.src('templates/' + template + '.scss')
+    .pipe(consolidate('lodash', options))
+    .pipe(rename({ basename:fontName }))
+    .pipe(gulp.dest('dist/')); // set path to export your SCSS
+    console.log(options)
+    // if you don't need sample.html, remove next 4 lines
+    gulp.src('templates/' + template + '.html')
+    .pipe(consolidate('lodash', options))
+    .pipe(rename({ basename:'index' }))
+    .pipe(gulp.dest('dist/')); // set path to export your sample HTML
+  })
+  .pipe(gulp.dest('dist/fonts')) // set path to export your fonts
+  .pipe(gulp.dest('dist/css/fonts')); // set path to export your fonts
 });
 
-gulp.task('clean', function () {
-  return gulp.src('dist/**/*')
-    .pipe(clean({force: true}));
+gulp.task('clean', function (done) {
+  rimraf('dist/**/*', function(err) {
+    done(err);
+  })
 });
+
+
+gulp.task('tag', function (done) {
+
+  
+
+
+});
+
 
 gulp.task('gp', function(done) {
   var ghpages = require('gh-pages');
@@ -66,12 +77,12 @@ gulp.task('gp', function(done) {
       done();
     }
 
-    });
+  });
 
 
 })
 
 
 gulp.task('watch', function(){
-  gulp.watch('*.sketch/Data', { debounceDelay: 3000 }, ['symbols']); // wait 3 sec after the last run
+  gulp.watch('*.sketch/**/*', { debounceDelay: 2000 }, ['clean']); // wait 3 sec after the last run
 });
